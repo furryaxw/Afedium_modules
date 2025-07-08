3e2eb7487846acd3386b46fd5db63a9b85135714
furryaxw <qianyucun07@163.com>
2025-07-09 02:16:45 +0800

    update

---
+++ fureyev1/fureye_driver.py
@@ -0,0 +1,49 @@
+from lib.lib import *
+import random
+
+default = {
+    "Limit": [-0.05, 0.05],
+    "Time": [0.5, 1]
+}
+
+
+Info = {
+    "name": "fureye_driver",
+    "id": "fureye_driver",
+    "version": "1.0.0",
+    "author": "Axw",
+    "url": "fureyev1/fureye_driver.py",
+    "description": "",
+    "type": "module",
+    "dependencies": ["fureyev1"],
+    "pip_dependencies": [],
+    "linux_dependencies": []
+}
+
+
+def __init__():
+    config = Config("display_pos", default)
+    config_t = Config("display")
+
+    while True:
+        time.sleep(0.1)
+        try:
+            if static["running"]["fureyev1"]:
+                break
+        except KeyError:
+            pass
+    mdata = []
+    for i in range(config_t.conf["num_display"]):
+        mdata.append(dynamic["eyes"][i]["eyeball"])
+        mdata[i]["enabled"] = True
+    static["running"][Info["id"]] = True
+    while static["running"][Info["id"]]:
+        x = random.uniform(config.conf["Limit"][0], config.conf["Limit"][1])
+        y = random.uniform(config.conf["Limit"][0], config.conf["Limit"][1])
+        schedule = time.time() + random.uniform(config.conf["Time"][0], config.conf["Time"][1])
+        while time.time() <= schedule:
+            for i in range(config_t.conf["num_display"]):
+                mdata[i]["x"] += (x - mdata[i]["nx"]) / 20
+                mdata[i]["y"] += (y - mdata[i]["ny"]) / 20
+            time.sleep(0.01)
+    return
--- template/module_template.py
+++ template/module_template.py
@@ -7,21 +7,26 @@ default = {
 }
 
 
-class Info:
-    name = "template"  # 模块名称
-    version = "1.0.0"  # 模块版本
-    url = ""  # 模块本体URL
-    dependencies = []  # 对其他模块依赖
-    pip_dependencies = []  # PIP库依赖
-    linux_dependencies = []  # Linux包依赖
+Info = {
+    "name": "模板",  # 模块名称
+    "id": "template",  # 模块ID
+    "version": "1.0.0",  # 模块版本
+    "author": "Axw",  # 模块作者
+    "url": "template/module_template.py",  # 模块URL
+    "description": "AFEDIUM模块示例模板",  # 模块简介
+    "type": "module",  # 模块类型
+    "dependencies": [],  # 对其他模块依赖
+    "pip_dependencies": [],  # PIP库依赖
+    "linux_dependencies": []  # Linux包依赖
+}
 
 
 def __init__():
     config = Config("template", default)
     # 初始化
 
-    static["running"][Info.name] = True  # 初始化完成，标注为已启动
-    while static["running"][Info.name]:
+    static["running"][Info["id"]] = True  # 初始化完成，标注为已启动
+    while static["running"][Info["id"]]:
         # 主循环
 
         time.sleep(1)
